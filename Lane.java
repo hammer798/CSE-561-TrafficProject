e00f1f06de93629a2563361822c5b935ddd7a49d
hammer798 <ibradley798@gmail.com>
2021-11-25 02:05:51 -0700

    Add initial Lane and Vehicle model files
    Initial commits for Lane and Vehicle models, notes added for later changes

---
+++ Lane.java
@@ -0,0 +1,87 @@
+//Vehicle.java 
+package TrafficProject.CSE-561-TrafficProject;
+
+import java.util.ArrayList;
+import GenCol.*;
+
+
+import model.modeling.*;
+
+
+public class Lane extends atomic{
+
+	int capacity = 20; //check notes for actual number-lanes modelled as 1/8 mile section of road
+	int currentSignal = 0; //0 is red, 1 is green, 2 is left turn
+	ArrayList<Vehicle> cars;
+	
+	public Lane(){
+	    super("Lane");
+	    addInport("newCar"); 
+	    addInport("trafficSignal"); //probably need one more inport from the traffic signal indicating capacity of adjacent streets
+	    addOutport("exitingCar");
+	    addOutport("currentVolume"); //both outports will go into intersection/traffic light, which will direct traffic to next target lane
+	}
+	//maybe add secondary constructor based on how we implement roads and lights(would take initial light signal)
+	
+	public void initialize(){
+		//Initialization:
+		phase = "notFull";
+	    cars = new ArrayList<Vehicle>();
+	    sigma = 1/0.0; //sigma: infinity
+	    super.initialize();
+	 }
+	
+	//External Transition Function
+	public void  deltext(double e, message x){
+	    Continue(e);
+	    for(int i=0; i<x.getLength(); i++) {
+			if(messageOnPort(x, "newCar", i)) {
+				entity val = x.getValOnPort("newCar", i);
+				Vehicle newCar = new Vehicle();
+				cars.add(newCar);
+				if(cars.size()==20) {
+					phase = "full";
+					holdIn("full", 1);
+				}
+			}
+	    }
+	    for(int i=0; i<x.getLength(); i++) {
+			if(messageOnPort(x, "trafficSignal", i)) {
+				entity val = x.getValOnPort("trafficSignal", i);
+				int signal = Integer.parseInt(val.getName());
+				currentSignal = signal;
+				holdIn("notFull", 1);
+			}
+	    }
+	}
+	
+	//Internal Transition Function:
+	public void  deltint( ){
+		holdIn(phase, 1); //one vehicle can exit a lane every second assuming there's space wherever they're going
+	}
+	
+	//not 100% sure this is necessary but probably good to have
+	public void deltcon(double e, message x) { 
+		deltint();
+		deltext(0, x);
+	}
+	
+	//Output Function:
+	public message out(){
+		message m = new message();
+		int chosenTurn = cars.get(0).getTurn();
+		if((trafficSignal == 1 && (chosenTurn == 1 || chosenTurn = 2) || (trafficSignal == 2 && chosenTurn == 0)){ //basically, does current traffic light match turn choice, will add capacity checks once implemented in traffic light
+			content con = new content("exitingCar", new entity(chosenTurn)); //pass turn so light knows where to send it
+			cars.remove(0);
+			phase = "notFull";
+			m.add(con);
+		content con2 = new content("currentVolume", new entity(cars.size()));
+		m.add(con2);
+	    return m;
+	}
+ 
+
+
+
+}
+
---
+++ Vehicle.java
@@ -0,0 +1,82 @@
+//Vehicle.java 
+package TrafficProject.CSE-561-TrafficProject;
+
+
+import GenCol.*;
+
+
+import model.modeling.*;
+
+
+public class Vehicle extends atomic{
+
+//state is a tuple of phase and turn
+protected int turn = 0; //0 is left, 1 is straight, 2 is right
+  
+public Vehicle(){
+    super("Vehicle");
+    addInport("newRoadSection"); 
+    addOutport("turn");
+}
+
+public void initialize(){
+	//Initialization:
+	phase = "noChoice";
+    turn = chooseTurn(); //set initial turn
+    sigma = 1/0.0; //sigma: infinity
+    super.initialize();
+ }
+
+//External Transition Function
+public void  deltext(double e, message x){
+    Continue(e);
+    for(int i=0; i<x.getLength(); i++) {
+		if(messageOnPort(x, "newRoadSection", i)) {
+			val = x.getValOnPort("newRoadSection", i);
+			turn = chooseTurn();
+			phase = "choiceMade";
+			holdIn("choiceMade", 5);//vehicle reevaluates choice every 5 seconds
+		}
+    }
+}
+
+//Internal Transition Function:
+public void  deltint( ){
+	if(phase == "choiceMade") {
+		phase = "noChoice";
+		holdIn("noChoice", 0.1); //takes .1 seconds to make new choice
+	}
+	if(phase == "noChoice") {
+		turn = chooseTurn();
+		phase = "choiceMade";
+		holdIn("choiceMade", 5);
+	}
+}
+
+//not 100% sure this is necessary but probably good to have
+public void deltcon(double e, message x) { 
+	deltint();
+	deltext(0, x);
+}
+
+//Output Function:
+public message out(){
+	message m = new message();
+	content con = new content("turn", new entity(turn));
+	m.add(con);
+    return m;
+}
+ 
+ public int chooseTurn() {
+	 //turn becomes random number between 0-2
+	 return 0;
+ }
+ 
+ public int getTurn() {
+	 return turn;
+ }
+
+
+
+}
+
